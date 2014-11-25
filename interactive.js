24909a0483cfd99fde0e3bf621327912c7966078
Isaac Ayodeji <deejecks@live.com>
2014-11-25 20:15:27 +0000

    Initializing repo for GH Pages

---
+++ interactive.js
@@ -0,0 +1,62 @@
+// Hot or Cold guess
+// User tries to guess the random number picked by the computer
+
+
+var getRandom = function () {
+	var randomNumber = Math.ceil(Math.random() * 100);
+	return randomNumber;
+}
+
+
+
+var computerChoice = getRandom();
+console.log( "secret number is: " + computerChoice );
+
+
+
+var oldDifference = 100;
+var newDifference;
+var userPick;
+var userChoice;
+var submit;
+
+var pick = function (e) {
+  e.preventDefault();
+  if ( !isNaN(userPick.value) && userPick.value >= 0 && userPick.value <= 100 ) {
+    userChoice = parseInt(userPick.value);
+    newDifference = Math.abs(userChoice - computerChoice);
+  }
+  else document.getElementById("user-feedback").innerHTML = "Please, make a valid entry";
+
+  if (newDifference == 0) {
+    document.getElementById("user-feedback").innerHTML = "You guessed right";
+  }
+  else if ( newDifference < oldDifference ) {
+    document.getElementById("user-feedback").innerHTML = "You're getting hotter";
+    oldDifference = newDifference;
+  }
+  else if ( newDifference > oldDifference ) {
+    document.getElementById("user-feedback").innerHTML = "You're getting colder";
+  }
+} // ends pick
+
+
+  
+
+var chooseNumber = function () {
+userPick = document.getElementById("user-entry");
+submit = document.getElementById("submit");
+
+submit.addEventListener("click", pick);
+
+
+} // ends chooseNumber
+
+
+
+chooseNumber();
+
+
+while (newDifference > 0) {
+  chooseNumber();
+}
---
+++ interactiveness.js
@@ -0,0 +1,61 @@
+// The Object Literal Pattern
+
+var gameController = {
+
+	getRandom: function () {
+    var randomNumber = Math.ceil(Math.random() * 100);
+    return randomNumber;
+  },
+
+  computerChoice: 0,
+  oldDifference: 100,
+  newDifference: NaN,
+  userPick: 0,
+  userChoice: 0,
+  submit: null,
+
+  init: function () {
+    this.computerChoice = this.getRandom();
+    console.log("secret number is: " + this.computerChoice);
+  },
+
+  pick: function (e) {
+    e.preventDefault();
+    if ( !isNaN(this.game.userPick.value) && this.game.userPick.value >= 0 && this.game.userPick.value <= 100 ) {
+      this.game.userChoice = parseInt(this.game.userPick.value);
+      this.game.newDifference = Math.abs(this.game.userChoice - this.game.computerChoice);
+    }
+    else document.getElementById("user-feedback").innerHTML = "Please, make a valid entry";
+
+    if (this.game.newDifference == 0) {
+      document.getElementById("user-feedback").innerHTML = "You guessed right";
+    }
+    else if ( this.game.newDifference < this.game.oldDifference ) {
+      document.getElementById("user-feedback").innerHTML = "You're getting hotter";
+      this.game.oldDifference = this.game.newDifference;
+    }
+    else if ( this.game.newDifference > this.game.oldDifference ) {
+      document.getElementById("user-feedback").innerHTML = "You're getting colder";
+    }
+  },
+
+  chooseNumber: function () {
+    this.userPick = document.getElementById("user-entry");
+    this.submit = document.getElementById("submit");
+    this.submit.game = this;
+
+    this.submit.addEventListener("click", this.pick);
+  }
+  
+};
+
+gameController.init();
+
+gameController.chooseNumber();
+
+
+while (gameController.newDifference > 0) {
+  gameController.chooseNumber();
+}
+
+
